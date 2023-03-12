dfc1c81db2789c1d750555232cfd3a996114d96a
Adriein <adria.claret@gmail.com>
2023-03-12 18:58:29 +0100

    removed test

--- ScreenAnalizerPackage/Stats/HitPoint.py
+++ ScreenAnalizerPackage/Stats/HitPoint.py
@@ -1,12 +1,11 @@
 import pyautogui
 from ScreenAnalizerPackage.Stats.Stat import Stat
-from ScreenAnalizerPackage.Shared.Screen import Screen
 
 
 class HitPoint(Stat):
     def find_stat_location(self) -> any:
         region = self.get_stat_roi()
-        Screen.roi_screenshot('Tmp/stat.png', region)
+
         return pyautogui.locateOnScreen(
             'Wiki/Stat/hp.png',
             confidence=0.8,
