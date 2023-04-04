7a129e3d10131f20d75d20a27baa91e5e03916d0
Adriein <adria.claret@gmail.com>
2023-04-04 16:26:12 +0200

    wip auto attack creatures

--- ScreenAnalizerPackage/WindowCapturer.py
+++ ScreenAnalizerPackage/WindowCapturer.py
@@ -5,4 +5,4 @@ from ScreenAnalizerPackage.Shared.Screen import Screen
 class WindowCapturer:
     @staticmethod
     def start() -> np.array:
-        return Screen.window_capture()
+        return Screen.window_capture(Screen.OBS_TIBIA_PREVIEW_WINDOW_ID)
