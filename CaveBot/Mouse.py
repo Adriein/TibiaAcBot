9cdd030d7f1996c17120f7f57cbe2de76c42ac28
Adriein <adria.claret@gmail.com>
2023-04-05 15:49:54 +0200

    wip auto attack creatures

--- CaveBot/Mouse.py
+++ CaveBot/Mouse.py
@@ -6,4 +6,7 @@ class Mouse:
     @staticmethod
     def use_left_button(coordinates: tuple[int, int]) -> None:
         (x, y) = coordinates
+        print(Screen.TIBIA_WINDOW_ID)
+        print(x)
+        print(y)
         Console.execute(f'xdotool windowmove --sync {Screen.TIBIA_WINDOW_ID} {x} {y} click 1')
