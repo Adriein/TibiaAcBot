ca7dbca966f68eda9542f32d39dac1070b3faef4
Adriein <adria.claret@gmail.com>
2023-04-07 16:06:02 +0200

    auto attack creatures

--- CaveBot/AutoAttack.py
+++ CaveBot/AutoAttack.py
@@ -4,7 +4,7 @@ from ScreenAnalizerPackage import Coordinate
 from LoggerPackage import Logger
 from .Player import Player
 import numpy as np
-
+import cv2
 
 class AutoAttack:
     @staticmethod
@@ -21,6 +21,15 @@ class AutoAttack:
             creature_coords_in_battle_list = self.battle_list.find_enemies(frame)
 
             nearest_creature, *_ = creature_coords_in_battle_list
+
+            if cv2.waitKey(1):
+                cv2.destroyAllWindows()
+
+                # draw the bounding box on the image
+            cv2.rectangle(frame, (nearest_creature.left, nearest_creature.top), (nearest_creature.left + nearest_creature.width, nearest_creature.top + nearest_creature.height), (255, 0, 0), 1)
+            # show the output image
+            cv2.imshow("Output", frame)
+            cv2.waitKey(0)
 
             if not self.battle_list.is_nearest_creature_attacked(frame, nearest_creature):
                 creature_click_coordinate = Coordinate.from_screen_region(nearest_creature)
