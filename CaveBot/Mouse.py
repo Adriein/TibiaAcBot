f28d6382ccde5751f19b7bd8657429b2e2b18778
Adriein <adria.claret@gmail.com>
2023-04-08 09:36:14 +0200

    auto attack creatures

--- CaveBot/Mouse.py
+++ CaveBot/Mouse.py
@@ -25,13 +25,14 @@ class Mouse:
         # Get the current pointer position
         original_pointer = root.query_pointer()
 
-        # Move the pointer to a new location
-        root.warp_pointer(coordinate.x + tibia_window_coords.x, coordinate.y + tibia_window_coords.y)
+        # Move the pointer to the target position
+        root.warp_pointer(original_pointer.root_x + coordinate.x - tibia_window_coords.x, original_pointer.root_y + coordinate.y - tibia_window_coords.y)
 
         # Send a left-click event
-        xtest.fake_input(disp, X.ButtonPress, 1)
+        xtest.fake_input(disp, X.ButtonPress, 1, X.NONE, 0, 0, 0)
         disp.sync()
-        xtest.fake_input(disp, X.ButtonRelease, 1)
+
+        xtest.fake_input(disp, X.ButtonRelease, 1, X.NONE, 0, 0, 0)
         disp.sync()
 
         # Console.execute(f'xdotool mousemove --window {Screen.TIBIA_WINDOW_ID} --sync {coordinate.x} {coordinate.y}')
