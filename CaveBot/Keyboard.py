dac2b688b2ac42dd92d47d3589bbbda9ef18da11
Adriein <adria.claret@gmail.com>
2023-04-09 18:56:34 +0200

    wip autolooting

--- CaveBot/Keyboard.py
+++ CaveBot/Keyboard.py
@@ -1,8 +1,8 @@
 from ConsolePackage.Console import Console
-from ScreenAnalizerPackage import Screen
 
 
 class Keyboard:
     @staticmethod
     def press(key: str):
-        Console.execute(f'xdotool windowfocus --sync {Screen.TIBIA_WINDOW_ID} key {key}')
+        pass
+        #Console.execute(f'xdotool windowfocus --sync {Screen.TIBIA_WINDOW_ID} key {key}')
