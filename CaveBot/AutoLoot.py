4e8b81b9d5dfd0f8277f56b0de0f4e1b81f7500e
Adriein <adria.claret@gmail.com>
2023-04-15 11:26:08 +0200

    wip looting area

--- CaveBot/AutoLoot.py
+++ CaveBot/AutoLoot.py
@@ -58,8 +58,8 @@ class AutoLoot:
 
                 cv2.rectangle(
                     frame,
-                    (looting_area.start_x + start_x, looting_area.start_y + start_y),
-                    (looting_area.end_x + end_x, looting_area.end_y + end_y),
+                    (looting_area.start_x + start_x, start_y),
+                    (end_x, end_y),
                     (255, 0, 255),
                     1
                 )
