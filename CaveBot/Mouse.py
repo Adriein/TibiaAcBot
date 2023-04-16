9aaf356e8fa40d42b2a4854dbc0d8c6c82d647cf
Adriein <adria.claret@gmail.com>
2023-04-16 18:32:42 +0200

    wip looting area

--- CaveBot/Mouse.py
+++ CaveBot/Mouse.py
@@ -8,14 +8,6 @@ class Mouse:
     LEFT_MOUSE_BUTTON = 1
     RIGHT_MOUSE_BUTTON = 2
 
-    __INSTANCE = None
-
-    def __new__(cls):
-        if Mouse.__INSTANCE:
-            return Mouse.__INSTANCE
-
-        Mouse.__INSTANCE = object.__new__(cls)
-
     def __init__(self):
         # Create a connection to the X server
         self.disp = display.Display()
