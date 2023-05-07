2b941b8f7faaac382755f0550ccba18ffe2d42bf
Adriein <adria.claret@gmail.com>
2023-05-07 19:59:12 +0200

    trying to get my own position in radar

--- CaveBot/PathFinder.py
+++ CaveBot/PathFinder.py
@@ -5,7 +5,6 @@ from ScreenAnalizerPackage import Scanner
 from FilesystemPackage import Cv2File
 from .MapCoordinate import MapCoordinate
 from ScreenAnalizerPackage import Coordinate
-from ScreenAnalizerPackage import ScreenRegion
 
 
 class PathFinder:
@@ -23,18 +22,9 @@ class PathFinder:
 
         height, width = mini_map_frame.shape
 
-        # extract the coordinates of the player position using cross needle
-
-        player_coordinates = self.__get_mini_map_player_position(mini_map_frame)
-
         # cut a portion of the map based on start coordinate
 
         coordinate = self.__get_pixel_from_coordinate(initial_map_coordinate)
-
-        print(str(coordinate))
-
-        print(math.floor(width/2))
-        print(math.floor(height/2))
 
         test_start_x = coordinate.x - (math.floor(width/2) + 20)
         test_end_x = coordinate.x + (math.floor(width/2) + 20)
@@ -61,25 +51,9 @@ class PathFinder:
 
     def __get_map_coordinate_from_pixel(self, coordinate: Coordinate, floor: int) -> MapCoordinate:
         return MapCoordinate(coordinate.x + 31744, coordinate.y + 30976, floor)
+
     def __get_pixel_from_coordinate(self, coordinate: MapCoordinate) -> Coordinate:
         return Coordinate(coordinate.x - 31744, coordinate.y - 30976)
-
-    def __get_mini_map_player_position(self, mini_map_frame: np.array) -> Coordinate:
-        map_position_cross = Cv2File.load_image(f'Wiki/Ui/Map/map_position_cross.png')
-        match = cv2.matchTemplate(mini_map_frame, map_position_cross, cv2.TM_CCOEFF_NORMED)
-
-        height, width = map_position_cross.shape
-
-        [_, max_coincidence, _, max_coordinates] = cv2.minMaxLoc(match)
-
-        (x, y) = max_coordinates
-
-        end_x = x + width
-        end_y = y + height
-
-        screen_region = ScreenRegion(x, end_x, y, end_y)
-
-        return Coordinate.from_screen_region(screen_region)
 
     def map_position_based_on_map_coordinate(self) -> None:
         map_coordinate = MapCoordinate(32063, 31884, 5)
