968abe7cf66e0449ccd2f0399c39b7620320075e
Adriein <adria.claret@gmail.com>
2023-05-20 19:44:36 +0200

    trying to map walkable waypoints to graph

--- PathFindingPackage/Map.py
+++ PathFindingPackage/Map.py
@@ -1,25 +1,19 @@
+from typing import Any
+
 import numpy as np
 import uuid
 import cv2
 import math
+import heapq
 from ScreenAnalizerPackage import Scanner
 from FilesystemPackage import Cv2File
 from ScreenAnalizerPackage import Coordinate
 from .Tile import Tile
 from .Waypoint import Waypoint
+from .MapGraph import MapGraph
 
 
 class Map:
-
-    def find_shortest_path(self, current_waypoint: str, destination_waypoint: str) -> list[Tile]:
-        current_waypoint = self.__string_to_waypoint(current_waypoint)
-        destination_waypoint = self.__string_to_waypoint(destination_waypoint)
-
-        self.__create_graph_between_waypoints(current_waypoint, destination_waypoint)
-
-        return []
-
-
     def where_am_i(self, last_waypoint: str, frame: np.array) -> Tile:
         last_waypoint = Waypoint(32063, 31884, 5)
         grey_scale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -59,32 +53,100 @@ class Map:
 
         return self.__get_map_tile_from_pixel(Coordinate(start_x, start_y), last_waypoint.z)
 
-    def __get_map_tile_from_pixel(self, coordinate: Coordinate, floor: int) -> Tile:
-        return Tile(uuid.uuid4(), coordinate.x + 31744, coordinate.y + 30976, floor)
+    def find_shortest_path(self, current_waypoint: str, destination_waypoint: str) -> list[Tile]:
+        current_waypoint = self.__string_to_waypoint(current_waypoint)
+        destination_waypoint = self.__string_to_waypoint(destination_waypoint)
 
-    def __get_pixel_from_waypoint(self, waypoint: Waypoint) -> Coordinate:
-        return Coordinate(waypoint.x - 31744, waypoint.y - 30976)
+        self.__create_graph_between_waypoints(current_waypoint, destination_waypoint)
 
-    def __create_graph_between_waypoints(self, current: Waypoint, destination: Waypoint):
+        return []
+
+    def __create_graph_between_waypoints(self, current: Waypoint, destination: Waypoint) -> Any:
         start_screen_coordinate = self.__get_pixel_from_waypoint(current)
         destination_screen_coordinate = self.__get_pixel_from_waypoint(destination)
 
-        # check in the walkable map the east, nort and west tile to see if its yellow or not
-        # if its not yellow its a walkable tile so i add it to the graph
+        initial_direction = self.__calculate_initial_direction(current, destination)
 
-    def map_position_based_on_map_coordinate(self) -> None:
-        waypoint = Waypoint(32063, 31884, 5)
+        # determine the direction where we have to start looking
+        # start a loop tracking the visited pixels and do the color logic on the pixels until reach the destination coordinate
 
-        coordinate = self.__get_pixel_from_waypoint(waypoint)
+        tibia_walkable_map = Cv2File.load_image(f'Wiki/Ui/Map/Walkable/floor-5-path.png', False)
 
-        tibia_map = Cv2File.load_image(f'Wiki/Ui/Map/Floors/floor-{waypoint.z}.png')
+        open_set = []
+        visited = set()
 
-        cv2.drawMarker(tibia_map, (coordinate.x, coordinate.y), (255, 0, 255), cv2.MARKER_CROSS, cv2.LINE_4)
+        start_tile = Tile.build(current)
+        start_tile.calculate_cost(destination)
 
-        test = tibia_map[coordinate.y - 100:coordinate.y + 100, coordinate.x - 100:coordinate.x + 100]
+        destination_tile = Tile.build(destination)
+
+        heapq.heappush(open_set, start_tile)
+
+        while open_set:
+            current_tile: Tile = heapq.heappop(open_set)
+
+            if current_tile == destination_tile:
+                # Goal reached, construct the path
+                path = []
+
+                while current_tile:
+                    path.append(current_tile)
+                    current_tile = current_tile.parent
+
+                path.reverse()
+
+                return path
+
+            visited.add(current_tile)
+
+
+
+            for neighbor_tile in current_tile.adjacent_tiles:
+                if neighbor_tile in visited:
+                    continue
+
+                if neighbor_tile.f_score < current_tile.f_score or neighbor_tile not in open_set:
+                    neighbor_tile.parent = current_tile
+                    neighbor.g_score = tentative_g_score
+
+                    if neighbor_tile not in open_set:
+                        open_set.append(neighbor_tile)
 
     def __string_to_waypoint(self, string_waypoint: str) -> Waypoint:
         x, y, z = string_waypoint.split(',')
 
         return Waypoint(x, y, z)
+
+    def __get_map_tile_from_pixel(self, coordinate: Coordinate, floor: int) -> Tile:
+        return Tile(uuid.uuid4(), Waypoint(coordinate.x + 31744, coordinate.y + 30976, floor))
+
+    def __get_pixel_from_waypoint(self, waypoint: Waypoint) -> Coordinate:
+        return Coordinate(waypoint.x - 31744, waypoint.y - 30976)
+
+    def __calculate_initial_direction(self, current: Waypoint, destination: Waypoint) -> str:
+        if destination.x > current.x:
+            if destination.y > current.y:
+                return 'south-east'
+
+            return 'north-east'
+
+        if destination.x == current.x:
+            if destination.y > current.y:
+                return 'south'
+
+            return 'north'
+
+        if destination.y > current.y:
+            return 'south-west'
+
+        return 'north-west'
+
+    def __create_adjacent_tiles(self, current_tile: Tile, destination: Tile) -> None:
+        # 0,1 stands for x (1, -1) means move left and right
+        # 2,3 stands for y (1, -1) means move up and down
+        cardinality = {0: 1, 1: -1, 2: 1, 3: -1}
+
+        for step in range(4):
+            next_waypoint = Waypoint(current_tile.waypoint.x + cardinality[step])
+            current_tile.add_adjacent_tile(Tile.build())
 
--- PathFindingPackage/Tile.py
+++ PathFindingPackage/Tile.py
@@ -1,23 +1,47 @@
+import uuid
 from uuid import UUID
+from .Waypoint import Waypoint
 
 
 class Tile:
-    def __init__(self, id: UUID, x: int, y: int, z: int):
-        self.id = id
-        self.x = x
-        self.y = y
-        self.z = z
+    @staticmethod
+    def build(waypoint: Waypoint) -> 'Tile':
 
-        self.adjacent_tiles = []
+        return Tile(uuid.uuid4(), waypoint)
+
+    def __init__(self, id: UUID, waypoint: Waypoint):
+        self.id = id
+        self.waypoint = waypoint
+
+        self.g_score = 1  # Cost from start node
+        self.h_score = 0  # Heuristic score
+        self.f_score = float('inf')  # Total score (g_score + h_score)
+        self.parent = None  # Parent node
+
+        self.adjacent_tiles: list[Tile] = []
+
+    def __lt__(self, other):
+        return self.f_score < other.f_score
 
     def __eq__(self, other):
         if isinstance(other, Tile):
-            return self.id == other.id and self.x == other.x and self.y == other.y and self.z == other.z
+            return self.waypoint.x == other.waypoint.x and self.waypoint.y == other.waypoint.y and self.waypoint.z == other.waypoint.z
 
         return False
 
     def __str__(self):
-        return f'Tile(x={self.x}, y={self.y}, z={self.z})'
+        return f'Tile(x={self.waypoint.x}, y={self.waypoint.y}, z={self.waypoint.z})'
 
-    def add_adjacent_tile(self, node: 'Tile'):
-        self.adjacent_tiles.append(node)
+    def set_parent(self, node: 'Tile'):
+        self.parent = node
+
+    def add_adjacent_tile(self, tile: 'Tile') -> None:
+        self.adjacent_tiles.append(tile)
+
+    def calculate_cost(self, destination: Waypoint):
+        self.h_score = self.__calculate_distance_between_two_points(destination)
+
+        self.f_score = self.h_score + self.g_score
+
+    def __calculate_distance_between_two_points(self, destination: Waypoint) -> float:
+        return ((destination.x - self.waypoint.x) ** 2 + (destination.y - self.waypoint.y) ** 2) ** 0.5
