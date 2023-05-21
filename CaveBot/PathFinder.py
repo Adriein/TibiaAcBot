b04fd6444daed32f67400694d94f0a8b466a8b86
Adriein <adria.claret@gmail.com>
2023-05-21 19:29:59 +0200

    trying to map walkable waypoints to graph

--- CaveBot/PathFinder.py
+++ CaveBot/PathFinder.py
@@ -1,12 +1,22 @@
 import numpy as np
 from PathFindingPackage import Map
+from .MoveCommand import MoveCommand
+import time
+from .Player import Player
 
 
 class PathFinder:
-    def execute(self, last_waypoint: str, next_waypoint: str, frame: np.array):
+    def execute(self, last_waypoint: str, next_waypoint: str, frame: np.array, player: Player):
         game_map = Map()
 
         path = game_map.find_shortest_path(last_waypoint, next_waypoint)
 
-        print(str(path))
+        while path.current is not None:
+            command: MoveCommand = path.current.data
+
+            time.sleep(0.6)
+
+            player.move(command)
+
+            path.next()
 
