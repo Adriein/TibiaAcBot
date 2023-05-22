4c718502c0b620370b22a348a3babdca25503f65
Adriein <adria.claret@gmail.com>
2023-05-22 19:20:34 +0200

    trying to map walkable waypoints to graph

--- CaveBot/PathFinder.py
+++ CaveBot/PathFinder.py
@@ -11,10 +11,6 @@ class PathFinder:
 
         path = game_map.find_shortest_path(last_waypoint, next_waypoint)
 
-        print(str(path))
-
-        raise Exception
-
         while path.current is not None:
             command: MoveCommand = path.current.data
 
