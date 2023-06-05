706fc1ac42d6ca70a5a0e4566ebcdd0f2bf45bee
Adriein <adria.claret@gmail.com>
2023-06-05 11:27:52 +0200

    trying to set game window position

--- CaveBot/PathFinder.py
+++ CaveBot/PathFinder.py
@@ -7,10 +7,10 @@ class PathFinder:
     def __init__(self):
         self.game_map = Map()
 
-    def execute(self, last_known_waypoint: str, destination_waypoint: str, frame: np.array) -> LinkedList:
-        current_tile = self.game_map.where_am_i(last_known_waypoint, frame)
+    def execute(self, last_known_waypoint: list[str], destination_waypoint: list[str], frame: np.array) -> LinkedList:
+        current_tile = self.game_map.where_am_i(last_known_waypoint[0], frame)
         print('where am i?')
         print(str(current_tile))
         current_waypoint = current_tile.waypoint.to_string()
 
-        return self.game_map.find_shortest_path(current_waypoint, destination_waypoint)
+        return self.game_map.find_shortest_path(current_waypoint, destination_waypoint[0])
