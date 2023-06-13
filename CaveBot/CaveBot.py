057193a61d719b929e7ddabd916a6c752f41b572
Adriein <adria.claret@gmail.com>
2023-06-13 19:46:20 +0200

    waypoints for thais wasp cave

--- CaveBot/CaveBot.py
+++ CaveBot/CaveBot.py
@@ -20,7 +20,7 @@ class CaveBot:
         combat_event = Event()
 
         # Thread(daemon=True, target=player.watch_mana).start()
-        cave_bot_script = Script.load('Wiki/Script/Thais/test_case.json')
+        cave_bot_script = Script.load('Wiki/Script/Thais/thais_wasp.json')
 
         auto_walk = AutoWalk(cave_bot_script, player)
 
