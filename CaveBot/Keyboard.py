08728f8a9b52585457498bcdecdf2a40eea82981
Adriein <adria.claret@gmail.com>
2023-06-18 13:11:30 +0200

    hability to chase runner opponents

--- CaveBot/Keyboard.py
+++ CaveBot/Keyboard.py
@@ -1,5 +1,3 @@
-from .Mouse import Mouse
-from ScreenAnalizerPackage import Coordinate
 from Xlib import X, display, XK
 from Xlib.protocol import event
 from ScreenAnalizerPackage import Screen
@@ -27,48 +25,6 @@ class Keyboard:
             detail=keycode
         )
         window.send_event(key_press_event, propagate=True)
-
-        key_release_event = event.KeyRelease(
-            time=time,
-            root=d.screen().root.id,
-            window=Screen.TIBIA_WINDOW_ID,
-            same_screen=0,
-            child=X.PointerRoot,
-            root_x=0, root_y=0, event_x=0, event_y=0,
-            state=0,
-            detail=keycode
-        )
-        d.send_event(Screen.TIBIA_WINDOW_ID, key_release_event, propagate=False)
-
-        # Sync to make sure the event is processed
-        d.flush()
-        # Console.execute(f'xdotool windowfocus --sync {Screen.TIBIA_WINDOW_ID} key {key}')
-
-    @staticmethod
-    def loot(coordinate: Coordinate) -> None:
-        # Create a connection to the X server
-        d = display.Display()
-
-        # Get the target window using its ID
-        window = d.create_resource_object('window', Screen.TIBIA_WINDOW_ID)
-
-        keycode = d.keysym_to_keycode(XK.string_to_keysym("Shift_L"))
-        time = X.CurrentTime
-        key_press_event = event.KeyPress(
-            time=time,
-            root=d.screen().root.id,
-            window=Screen.TIBIA_WINDOW_ID,
-            same_screen=0,
-            child=X.PointerRoot,
-            root_x=0, root_y=0, event_x=0, event_y=0,
-            state=0,
-            detail=keycode
-        )
-        window.send_event(key_press_event, propagate=True)
-
-        mouse = Mouse()
-
-        mouse.use_right_button(coordinate)
 
         key_release_event = event.KeyRelease(
             time=time,
