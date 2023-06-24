9108f3a12f170f3b78d406cf463756cb704b37a6
Adriein <adria.claret@gmail.com>
2023-06-24 15:16:20 +0200

    created amazon camp script

--- start-tibia.sh
+++ start-tibia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Open the first terminal tab, navigate to directory and run command
-gnome-terminal --tab --working-directory="/home/aclaret/Programs/Tibia" --command="bash -c './start ; exec bash'"
+gnome-terminal --tab --working-directory="/home/aclaret/Programs/Tibia" --command="bash -c './start-tibia-launcher.sh ; exec bash'"
 
 # Open the second terminal tab, navigate to another directory and run command
 gnome-terminal --tab --working-directory="/home/aclaret/CLARET-DEV/TibiaAcBot"
