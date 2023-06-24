d3be344c2e39eb8ca38590ed98063c06822e8361
Adriein <adria.claret@gmail.com>
2023-06-24 12:43:12 +0200

    created amazon camp script

---
+++ start-tibia.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Open the first terminal tab, navigate to directory and run command
+gnome-terminal --tab --working-directory="/home/aclaret/Programs/Tibia" --command="bash -c './start ; exec bash'"
+
+# Open the second terminal tab, navigate to another directory and run command
+gnome-terminal --tab --working-directory="/home/aclaret/CLARET-DEV/TibiaAcBot"
+
+# Open OBS
+obs
