738791429360027bfa8b59eb37aeba4335558a8a
Adriein <adria.claret@gmail.com>
2023-06-25 10:42:09 +0200

    started training module

--- App.py
+++ App.py
@@ -1,9 +1,9 @@
-import time
 from LoggerPackage import Logger as TibiaAcBotLogger
 from ScreenAnalizerPackage.Shared.Screen import Screen
 from ScreenAnalizerPackage.Stats.Stat import Stat
 from CaveBot.CaveBot import CaveBot
 from dotenv import load_dotenv
+import argparse
 
 
 class TibiaAcBot:
@@ -11,6 +11,8 @@ class TibiaAcBot:
     def init():
         try:
             TibiaAcBot.setup_global()
+
+            TibiaAcBot.collect_program_arguments()
 
             TibiaAcBotLogger.info('Started...')
             TibiaAcBotLogger.info('Press Ctrl+C to stop the execution')
@@ -36,6 +38,16 @@ class TibiaAcBot:
 
         Screen.setup_global_variables()
         Stat.setup_global_variables()
+
+    @staticmethod
+    def collect_program_arguments() -> None:
+        TibiaAcBotLogger.info('Collecting program arguments...')
+
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument('--train', type=str, help='Indicate that program should start in training mode')
+
+        print(parser.parse_args())
 
 
 TibiaAcBot().init()
