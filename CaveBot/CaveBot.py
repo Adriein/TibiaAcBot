9dc883d3c4a122045e7cd2a72cb0048aea51294a
Adriein <adria.claret@gmail.com>
2023-07-22 11:06:14 +0200

    swamp troll cave

--- CaveBot/CaveBot.py
+++ CaveBot/CaveBot.py
@@ -7,6 +7,7 @@ from .AutoWalk import AutoWalk
 from .AutoHealer import AutoHealer
 from .Script import Script
 from threading import Thread, Event
+import pyautogui
 
 
 class CaveBot:
@@ -23,6 +24,8 @@ class CaveBot:
         # cave_bot_script = Script.load('Wiki/Script/Thais/thais_wasp.json')
         cave_bot_script = Script.load('Wiki/Script/Venore/swamp_troll_cave.json')
         # cave_bot_script = Script.load('Wiki/Script/Venore/swampling_cave_floor_10.json')
+        print(pyautogui.size())
+        raise Exception
 
         auto_loot = AutoLoot(player, Screen.GAME_WINDOW)
 
