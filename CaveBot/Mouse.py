8955aa0eda6e7a706e9f5c6bb4e6b3c2ce66a230
Adriein <adria.claret@gmail.com>
2023-07-22 11:22:09 +0200

    swamp troll cave

--- CaveBot/Mouse.py
+++ CaveBot/Mouse.py
@@ -3,15 +3,13 @@ import pyautogui
 
 
 class Mouse:
-    def use_right_button(self, coordinate: Coordinate) -> None:
-        py_auto_gui_y = (coordinate.y * -1) - 768/2
-        py_auto_gui_x = coordinate.x + 1366/2
+    def move(self, coordinate: Coordinate) -> None:
+        py_auto_gui_y = (coordinate.y * -1) - 768 / 2
+        py_auto_gui_x = coordinate.x + 1366 / 2
 
         pyautogui.moveTo(py_auto_gui_x, py_auto_gui_y)
-
+    def use_right_button(self) -> None:
         pyautogui.click(button='right')
 
-    def use_left_button(self, coordinate: Coordinate) -> None:
-        pyautogui.moveTo(coordinate.x, coordinate.y)
-
+    def use_left_button(self) -> None:
         pyautogui.click(button='left')
--- CaveBot/Player.py
+++ CaveBot/Player.py
@@ -39,21 +39,25 @@ class Player:
         # self.mouse.use_left_button(coordinates)
 
     def precision_attack(self, coordinates: Coordinate) -> None:
-        self.mouse.use_left_button(coordinates)
+        self.mouse.move(coordinates)
+        self.mouse.use_left_button()
 
     def rope(self, coordinates: Coordinate) -> None:
         Keyboard.press('f')
-        self.mouse.use_left_button(coordinates)
+        self.mouse.move(coordinates)
+        self.mouse.use_left_button()
 
     def use_hand_stair(self, coordinates: Coordinate) -> None:
-        self.mouse.use_right_button(coordinates)
+        self.mouse.move(coordinates)
+        self.mouse.use_right_button()
 
     def eat(self) -> None:
         Keyboard.press('v')
 
     def loot(self, coordinates: Coordinate) -> None:
+        self.mouse.move(coordinates)
         Keyboard.key_down('shift')
-        self.mouse.use_right_button(coordinates)
+        self.mouse.use_right_button()
         Keyboard.key_up('shift')
 
     def chase_opponent(self) -> None:
