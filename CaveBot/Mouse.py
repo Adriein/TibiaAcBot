591970549a2cacefac2f052cf1327d916630ca67
Adriein <adria.claret@gmail.com>
2023-07-22 11:55:25 +0200

    swamp troll cave

--- CaveBot/Mouse.py
+++ CaveBot/Mouse.py
@@ -4,7 +4,7 @@ import pyautogui
 
 class Mouse:
     def move(self, coordinate: Coordinate) -> None:
-        pyautogui.moveTo(coordinate.x + 20, coordinate.y)
+        pyautogui.moveTo(coordinate.x + 40, coordinate.y)
 
     def use_right_button(self) -> None:
         pyautogui.click(button='right')
