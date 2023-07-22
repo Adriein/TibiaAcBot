926e48b3c4c957678679d64ef4f6c7e499448e0c
Adriein <adria.claret@gmail.com>
2023-07-22 11:11:38 +0200

    swamp troll cave

--- CaveBot/CaveBot.py
+++ CaveBot/CaveBot.py
@@ -24,8 +24,6 @@ class CaveBot:
         # cave_bot_script = Script.load('Wiki/Script/Thais/thais_wasp.json')
         cave_bot_script = Script.load('Wiki/Script/Venore/swamp_troll_cave.json')
         # cave_bot_script = Script.load('Wiki/Script/Venore/swampling_cave_floor_10.json')
-        print(pyautogui.size())
-        raise Exception
 
         auto_loot = AutoLoot(player, Screen.GAME_WINDOW)
 
--- CaveBot/Mouse.py
+++ CaveBot/Mouse.py
@@ -4,7 +4,10 @@ import pyautogui
 
 class Mouse:
     def use_right_button(self, coordinate: Coordinate) -> None:
-        pyautogui.moveTo(coordinate.x, coordinate.y)
+        py_auto_gui_y = (coordinate.y * -1) + 768/2
+        py_auto_gui_x = coordinate.x - 1366/2
+
+        pyautogui.moveTo(py_auto_gui_x, py_auto_gui_y)
 
         pyautogui.click(button='right')
 
