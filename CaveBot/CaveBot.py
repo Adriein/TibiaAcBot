2603b681157469e78c40f9b6fad74059630ae52b
Adriein <adria.claret@gmail.com>
2023-08-06 10:45:39 +0200

    removed health watch since is easier to heal in every battle

--- CaveBot/CaveBot.py
+++ CaveBot/CaveBot.py
@@ -31,17 +31,11 @@ class CaveBot:
 
         auto_attack = AutoAttack(auto_loot, player, walking_event, combat_event, cave_bot_script.creatures)
 
-        auto_healer = AutoHealer(player, combat_event)
-
         attack_thread = Thread(daemon=True, target=auto_attack.attack)
 
         walk_thread = Thread(daemon=True, target=auto_walk.start)
 
-        health_thread = Thread(daemon=True, target=auto_healer.heal)
-
         attack_thread.start()
-
-        health_thread.start()
 
         walk_thread.start()
 
