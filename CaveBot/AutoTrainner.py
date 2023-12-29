23e813f20b50f6ff5f2c0f84e9d91f498f55c09e
Adriein <adria.claret@gmail.com>
2023-12-29 14:33:22 +0100

    changed auto trainer

--- CaveBot/AutoTrainner.py
+++ CaveBot/AutoTrainner.py
@@ -4,23 +4,43 @@ from .TrainingAutoAttack import TrainingAutoAttack
 from .AutoHealer import AutoHealer
 from .Script import Script
 from threading import Thread, Event
+import time
+import random
 
 
 class AutoTrainner:
+    def __init__(self):
+        pass
+
     def train(self) -> None:
         TibiaAcBotLogger.info('AutoTraining starting')
 
-        player = Player.create()
+        # player = Player.create()
 
-        combat_event = Event()
+        # combat_event = Event()
 
-        training_script = Script.load('Wiki/Script/Training/training.json')
+        # training_script = Script.load('Wiki/Script/Training/training.json')
 
-        auto_attack = TrainingAutoAttack(player, training_script.creatures, combat_event)
+        # auto_attack = TrainingAutoAttack(player, training_script.creatures, combat_event)
 
-        attack_thread = Thread(daemon=True, target=auto_attack.train)
+        # attack_thread = Thread(daemon=True, target=auto_attack.train)
 
-        attack_thread.start()
+        # attack_thread.start()
 
+        # while True:
+        # pass
         while True:
-            pass
+            # Generate a random sleep time between 5 and 15 seconds
+            sleep_duration = generate_random_sleep_time()
+
+            # Sleep for the randomly generated time
+            time.sleep(sleep_duration * 60)
+
+            # Log a message to the console
+            TibiaAcBotLogger.info(f'Slept for {sleep_duration} seconds. Eating and casting spells')
+            player = Player.create()
+            player.eat()
+            player.heal()
+
+    def generate_random_sleep_time(self):
+        return random.randint(1, 15)
